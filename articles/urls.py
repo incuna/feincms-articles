46c655ff445b6736d34d47043c09d1fec1267899
max@173a7045-7439-0410-a177-ade7cbf61f2d <max@173a7045-7439-0410-a177-ade7cbf61f2d>
2010-04-08 16:57:14 +0000

    Article tags

--- articles/urls.py
+++ articles/urls.py
@@ -2,23 +2,9 @@ from django.conf import settings
 from django.conf.urls.defaults import *
 from models import Article#, Category
 
-urlpatterns = patterns(
-    'tagging.views',
-    url(r'^tag/(?P<tag>[^/]+)/$', 'tagged_object_list', 
-        dict(queryset_or_model=Article, 
-             #paginate_by=10, 
-             allow_empty=True,
-             #template_object_name='articles',
-             template_name='articles/article_list.html',
-             extra_context={'category': None},
-            ),
-        name="article_tag"),
-                      )
-
-urlpatterns += patterns('articles.views',
+urlpatterns = patterns('articles.views',
     url(r'^(?P<category_url>[a-z0-9_/-]+/)(?P<slug>[a-z0-9_-]+).html$', 'article_detail', name="article_detail"),
     url(r'^(?P<category_url>[a-z0-9_/-]+/)$', 'article_category', name='article_category'),
     url(r'^$', 'article_category', name='article_index'),
-    
-) 
+)
 
--- articles/views.py
+++ articles/views.py
@@ -2,6 +2,7 @@
 from django.template import RequestContext
 from django.shortcuts import get_object_or_404, render_to_response
 from models import Article, Category
+from tagging.models import Tag, TaggedItem
 
 
 def article_detail(request, category_url, slug, extra_context=None):
@@ -10,12 +11,14 @@ def article_detail(request, category_url, slug, extra_context=None):
 
     article = get_object_or_404(Article.objects.active(), category__local_url=category_url, slug=slug)
 
+    tags = Tag.objects.usage_for_queryset(Article.objects.filter(category__in=article.category.get_descendants(include_self=True)))
 
     if extra_context is not None:
         contect.update(extra_context)
 
     context.update({
         'object': article,
+        'tags': tags,
     })
     
     return render_to_response('articles/article_detail.html', context)
@@ -31,11 +34,23 @@ def article_category(request, category_url=None, extra_context=None):
     else:
         category = None
 
+    tags = Tag.objects.usage_for_queryset(articles)
+    tag = None
+    if request.GET and 'tag' in request.GET:
+        try:
+            tag = Tag.objects.get(name=request.GET['tag'])
+        except Tag.DoesNotExist:
+            pass
+        else:
+            articles = TaggedItem.objects.get_union_by_model(articles, [tag])
+
     if extra_context is not None:
         contect.update(extra_context)
 
     context.update({
         'object_list': articles,
+        'tags': tags,
+        'tag': tag,
         'category': category,
     })
     
