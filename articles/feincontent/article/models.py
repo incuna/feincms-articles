31813386aeb47d5b51b003bebbeb791335939723
james@173a7045-7439-0410-a177-ade7cbf61f2d <james@173a7045-7439-0410-a177-ade7cbf61f2d>
2010-09-29 13:13:40 +0000

    Removed lots of code in favour if fkeycontent_factory

--- articles/feincontent/article/models.py
+++ articles/feincontent/article/models.py
@@ -1,30 +1,11 @@
-from django.db import models
-from django.conf import settings
-from django.core.exceptions import ImproperlyConfigured
-from django.template import RequestContext
-from django.template.loader import render_to_string
+from incunafein.content import BaseFkeyContent
 
-class ArticleContent(models.Model):
-
+class ArticleFkeyContent(BaseFkeyContent):
+    
     class Meta:
         abstract = True
-
-    @classmethod
-    def initialize_type(cls):
-        if 'articles' not in settings.INSTALLED_APPS:
-            raise ImproperlyConfigured, "You have to add 'articles' to your INSTALLED_APPS before creating a %s" % cls.__name__
-
-        cls.add_to_class('category', models.ForeignKey('articles.Category',
-                                                       related_name='%s_%s_set' % (cls._meta.app_label, cls._meta.module_name)
-        ))
-
-    def render(self, request=None, **kwargs):
-        if request is not None:
-            context = RequestContext(request)
-            context.update({ 'content': self })
-        else:
-            context = { 'content': self }
-
-        return render_to_string(['content/articles/category_%s.html' % self.category.slug,
-                                 'content/articles/default.html',
-                                ], context)
+            
+    def template_hierarchy(self):
+        return ['content/articles/category_%s.html' % self.category.slug,
+                'content/articles/default.html',
+               ]
