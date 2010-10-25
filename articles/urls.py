803f0bc4dfa6fae92b12b63939396677a7b2e8fb
allan@173a7045-7439-0410-a177-ade7cbf61f2d <allan@173a7045-7439-0410-a177-ade7cbf61f2d>
2010-10-25 12:51:49 +0000

    Revert my change

--- articles/urls.py
+++ articles/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('articles.views',
     url(r'^(?P<category_url>[a-z0-9_/-]+/)(?P<slug>[a-z0-9_-]+).html$', 'article_detail', name="article_detail"),
     url(r'^(?P<category_url>[a-z0-9_/-]+/)$', 'article_category', name='article_category'),
-    url(r'^$', 'article_category', name='article_index', kwargs={'category_url': 'news-and-press-releases/'}),
+    url(r'^$', 'article_category', name='article_index'),
 )
 
