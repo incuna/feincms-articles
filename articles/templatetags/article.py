6ea0d0a81866d9a86e00de1df61cc287e60e6e32
max@173a7045-7439-0410-a177-ade7cbf61f2d <max@173a7045-7439-0410-a177-ade7cbf61f2d>
2010-12-03 11:44:37 +0000

    Not displaying categories that are empty

--- articles/templatetags/article.py
+++ articles/templatetags/article.py
@@ -8,24 +8,6 @@ from incuna.template.defaulttags import parse_tokens
 
 register = template.Library()
 
-#@register.inclusion_tag('articles/categories.html')
-#def categories(selected=None, current=None):
-#    categories = None
-#    if current is None:
-#        categories = Category.objects.filter(parent__isnull=True)
-#    else:
-#        if selected is not None:
-#            # is the selected category a descendant of 
-#            if current.get_descendants(include_self=True).filter(pk=selected.pk).count() > 0:
-#                categories = current.children.all()
-
-#    if categories is not None:
-#        categories = categories.filter(article__isnull=False).distinct()
-
-#    return {'selected': selected,
-#            'categories': categories,
-#           }
-
 class CategoriesNode(template.Node):
     """
         Output a list of categories.
