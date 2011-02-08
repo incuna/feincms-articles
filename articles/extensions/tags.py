7f645a43c784c6dedf6fa80172ef431e07f63929
Max Peterson <max@incuna.com>
2011-02-08 09:24:00 +0000

    Translation support

--- articles/extensions/tags.py
+++ articles/extensions/tags.py
@@ -1,12 +1,9 @@
 from django.utils.translation import ugettext_lazy as _
 from django.conf.urls.defaults import patterns, url
-#from tagging.fields import TagField
 from taggit.managers import TaggableManager
 
 def register(cls, admin_cls):
-    #cls.add_to_class('tags', TagField(_('tags'), null=True, blank=True))
-
-    cls.add_to_class('tags', TaggableManager())
+    cls.add_to_class('tags', TaggableManager(verbose_name=_('tags')))
 
     cls.urlpatterns += patterns('taggit.views',
                                 url(r'^tags/(?P<slug>[a-z0-9_-]+)/$', 'tagged_object_list', {'queryset': cls.objects.active()}, name="article_tagged_list"),
