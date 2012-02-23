700b7d5db02087148ade90846558f83ab8088151
Marc Tamlyn <marc.tamlyn@gmail.com>
2012-02-23 18:44:45 +0000

    Add a simple content type to show articles.
    Shows a list of articles - to be used as a link through.

---
+++ articles/content.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.template.loader import render_to_string
+
+from models import Article
+
+
+class ArticleList(models.Model):
+    number = models.IntegerField()
+
+    class Meta:
+        abstract = True
+
+    def get_queryset_for_render(self):
+        return Article.objects.all()
+
+    def render(self, **kwargs):
+        context = {
+            'object_list': self.get_queryset_for_render()[:self.number],
+            'request': kwargs.get('request'),
+        }
+        return render_to_string('content/articles/list.html', context)
