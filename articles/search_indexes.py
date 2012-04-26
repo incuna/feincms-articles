aea07390f9f26cb5679fe6dfcef01fceceb8f939
Charlie Denton <charlie@meshy.co.uk>
2012-04-26 11:34:08 +0100

    Support haystack version 2.

--- articles/search_indexes.py
+++ articles/search_indexes.py
@@ -1,14 +1,20 @@
-from haystack.indexes import *
-from haystack import site
+from haystack import indexes
 from models import Article
 
-class ArticleIndex(SearchIndex):
-    name = CharField(model_attr='title')
-    text = CharField(document=True, use_template=True)
+
+class ArticleIndex(indexes.SearchIndex):
+    name = indexes.CharField(model_attr='title')
+    text = indexes.CharField(document=True, use_template=True)
 
     def get_queryset(self):
         """Used when the entire index for model is updated."""
         return Article.objects.active()
 
-site.register(Article, ArticleIndex)
 
+# In haystack < 2.0 we need to explicitly register indexes
+try:
+    from haystack import site
+except ImportError:
+    pass
+else:
+    site.register(Article, ArticleIndex)
