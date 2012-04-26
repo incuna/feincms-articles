3bfd6cadea817df0730ee4d61cb9df9e9569acbe
Charlie Denton <charlie@meshy.co.uk>
2012-04-26 12:30:26 +0100

    Follow haystack v2 docs.

--- articles/search_indexes.py
+++ articles/search_indexes.py
@@ -6,9 +6,12 @@ class ArticleIndex(indexes.SearchIndex):
     name = indexes.CharField(model_attr='title')
     text = indexes.CharField(document=True, use_template=True)
 
+    def get_model(self):
+        return Article
+
     def get_queryset(self):
         """Used when the entire index for model is updated."""
-        return Article.objects.active()
+        return self.get_model().objects.active()
 
 
 # In haystack < 2.0 we need to explicitly register indexes
