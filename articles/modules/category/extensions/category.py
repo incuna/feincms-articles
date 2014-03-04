1754419f4a4371439bf1eab64c04cf742096f630
David Zaltron <d.zaltron@gmail.com>
2014-03-04 16:51:18 +0100

    Django 1.6 compatibility import fix

--- articles/modules/category/extensions/category.py
+++ articles/modules/category/extensions/category.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from feincms.content.application import models as app_models
